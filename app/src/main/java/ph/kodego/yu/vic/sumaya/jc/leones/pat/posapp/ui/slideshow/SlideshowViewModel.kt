74598e45e1dd4ff2ac54a046aa5f2ba806b2c88d
PatriciaIvee <patriciaiveeleones@gmail.com>
2023-03-24 14:46:01 +0800

    Initial commit

---
+++ app/src/main/java/ph/kodego/yu/vic/sumaya/jc/leones/pat/posapp/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package ph.kodego.yu.vic.sumaya.jc.leones.pat.posapp.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
