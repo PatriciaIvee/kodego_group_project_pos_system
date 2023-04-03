7cded94e24933ed4d489d337eca0acd66a226482
PatriciaIvee <patriciaiveeleones@gmail.com>
2023-04-03 18:35:37 +0800

    APR 03, 2023 Added Item and Order Model

--- app/src/main/java/ph/kodego/yu/vic/sumaya/jc/leones/pat/posapp/model/Item.kt
+++ app/src/main/java/ph/kodego/yu/vic/sumaya/jc/leones/pat/posapp/model/Item.kt
@@ -1,9 +1,13 @@
 package ph.kodego.yu.vic.sumaya.jc.leones.pat.posapp.model
 
-class Item {
+open class Item {
     var itemName: String = ""
     var itemType: String = ""
     var itemPrice: Float = 0.0f
-    var itemQuantity: Int = 0
     var itemStock: Int = 0
+}
+
+class Order: Item(){
+    var orderQuantity: Int = 0
+
 }
