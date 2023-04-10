20e31ef5772517205f69ad4e7b3965759bcd3f5f
PatriciaIvee <patriciaiveeleones@gmail.com>
2023-04-10 16:58:12 +0800

    APR 10, 2023 Added swipe function on orderList recycler view Added badge from OrderListFragment.kt to OrdersFragment.kt and the nav drawer Removed unnecessary code on OrderListViewModel.kt

--- app/src/main/java/ph/kodego/yu/vic/sumaya/jc/leones/pat/posapp/ui/orders/OrderListViewModel.kt
+++ app/src/main/java/ph/kodego/yu/vic/sumaya/jc/leones/pat/posapp/ui/orders/OrderListViewModel.kt
@@ -13,20 +13,4 @@ class OrderListViewModel: ViewModel() {
     val orders: MutableLiveData<List<Order>?>
         get() = _orders
 
-    fun setTotalOrderQuantity(quantity: Int) {
-        totalOrderQuantity.value = quantity
-    }
-
-    init {
-        _orders.value = mutableListOf()
-        totalOrderQuantity.value = 0 // Initialize to 0 here
-    }
-
-    private fun getTotalOrderQuantity(orders: List<Order>): Int {
-        var totalQuantity = 0
-        for (order in orders) {
-            totalQuantity += order.orderQuantity
-        }
-        return totalQuantity
-    }
 }
