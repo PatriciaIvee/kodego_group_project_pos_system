6031fed25b1f98bb2b34e857180ae4a3830b8da5
Juan Carlos Sumaya <sumaya.jcs@gmail.com>
2023-05-04 20:43:07 +0800

    updated ReceiptsFragment.kt

--- app/src/main/java/ph/kodego/yu/vic/sumaya/jc/leones/pat/posapp/ui/receipts/ReceiptsFragment.kt
+++ app/src/main/java/ph/kodego/yu/vic/sumaya/jc/leones/pat/posapp/ui/receipts/ReceiptsFragment.kt
@@ -6,18 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -117,21 +118,31 @@ class ReceiptsFragment : Fragment() {
 
             Row(
                 modifier = Modifier
-                    .align(Alignment.BottomCenter)
                     .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Button(
-                    onClick = { /* Left button action */ },
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Text(text = "Left")
-                }
-                Button(
-                    onClick = { /* Right button action */ },
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Text(text = "Right")
-                }
+                Icon(
+                    painter = painterResource(
+                        id = ph.kodego.yu.vic.sumaya.jc.leones.pat.posapp.R.drawable.ic_baseline_arrow_back_24),
+                    contentDescription = "Back button",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(40.dp)
+                        .clickable(onClick = { /* Back button action */ })
+                )
+                Icon(
+                    painter = painterResource(
+                        id = ph.kodego.yu.vic.sumaya.jc.leones.pat.posapp.R.drawable.ic_baseline_arrow_forward_24),
+                    contentDescription = "Next button",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(40.dp)
+                        .clickable(onClick = { /* Next button action */ })
+                )
             }
         }
     }
